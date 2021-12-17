49159fb9f8add6580acc6c9b5f644e0f139f0d62
MK202019 <91296207+MK202019@users.noreply.github.com>
2021-12-17 10:10:57 +0900

    a

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const io = require('socket.io')(http)
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
+
+const dotenv = require('dotenv')
+dotenv.config()
 const host = process.env.HOST
 const port = process.env.PORT
 
