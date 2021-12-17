5f89df20ff3b30dd3de0a45bccce86ac5418b0b6
MK202019 <91296207+MK202019@users.noreply.github.com>
2021-12-17 11:42:25 +0900

    chat

---
+++ public/js/chat.js
@@ -0,0 +1,25 @@
+$(() => {
+    const url = ''
+    let socket = io.cnnect(url)
+
+    const message = $('#message')
+    const chatList = $('#chatList')
+
+    $('#send').on('click',()=> {
+        if(!message.val())return
+
+        socket.emit('message',
+        {
+            message: message.val()
+        })
+
+        message.val('')
+    })
+
+    socket.on('message',(data) => {
+        console.log(data)
+        let chatElement = $('<p>').append(data.message)
+        console.log(chatElement)
+        chatList.prepend(chatElement)
+    })
+})
